dae730c4b05865b87d9d6c4d69dbf586bcd1c067
Kendell R <KTibow@users.noreply.github.com>
2025-02-27 16:55:59 -0800

    Update uqm-debug.data.js

--- functions/uqm-debug.data.js
+++ functions/uqm-debug.data.js
@@ -1,3 +1,3 @@
 export async function onRequestGet(context) {
-  return await fetch("https://page.com")
+  return await fetch("https://pub-74224d70bb9242ca8d3952ba8d4c0446.r2.dev/uqm-debug.data")
 }
